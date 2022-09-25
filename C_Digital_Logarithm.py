9ba10c1eae95637e60f79a87d8604be0988eda71
RISHAV KUMAR DUBEY <rishavkumar21012001@gmail.com>
2022-09-25 22:59:55 +0530

    Initial commit

---
+++ C_Digital_Logarithm.py
@@ -0,0 +1,13 @@
+for _ in range(int(input())):
+    ans=0
+    n=int(input())
+    l1=list(map(int,input().split()))
+    l2=list(map(int,input().split()))
+    for x in l2:
+        if x in l1:
+            l1.remove(x)
+        else:
+            if x not in l1:
+                ans+=1
+            ans+=1
+    print(ans)
